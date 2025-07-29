d2c7b009c9c37ac26fd545404ddfd069fdb788bc
Baylo <microsoftbaylo@laposte.net>
2025-07-29 15:49:57 +0200

    feat: create Lane entity, repository, and Turbo Streams template for real-time updates

---
+++ src/Entity/Lane.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LaneRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\UX\Turbo\Attribute\Broadcast;
+
+#[ORM\Entity(repositoryClass: LaneRepository::class)]
+#[Broadcast]
+class Lane
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $sortOrder = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSortOrder(): ?int
+    {
+        return $this->sortOrder;
+    }
+
+    public function setSortOrder(int $sortOrder): static
+    {
+        $this->sortOrder = $sortOrder;
+
+        return $this;
+    }
+}
