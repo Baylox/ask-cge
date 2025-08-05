b683a6e294222be171edf97571011a230c20ba05
Baylo <microsoftbaylo@laposte.net>
2025-08-05 11:46:30 +0200

    feat: add LaneFactory for creating Lane entities with default values

---
+++ src/Factory/LaneFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Lane;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Lane>
+ */
+final class LaneFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Lane::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'sortOrder' => self::faker()->numberBetween(1, 32767),
+            'title' => self::faker()->text(50),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Lane $lane): void {})
+        ;
+    }
+}
