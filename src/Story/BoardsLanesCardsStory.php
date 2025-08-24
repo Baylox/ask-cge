be9c68be0cba35c0c693ad4618e510a259c9b894
Baylo <microsoftbaylo@laposte.net>
2025-08-24 02:36:52 +0200

    fix: improve code documentation in BoardsLanesCardsStory

--- src/Story/BoardsLanesCardsStory.php
+++ src/Story/BoardsLanesCardsStory.php
@@ -9,6 +9,8 @@ use Zenstruck\Foundry\Story;
 
 final class BoardsLanesCardsStory extends Story
 {
+
+     // This method creates 2 boards, each containing 3 lanes.
     public function build(): void
     {
         $boards = BoardFactory::createMany(2);
@@ -16,6 +18,7 @@ final class BoardsLanesCardsStory extends Story
         foreach ($boards as $board) {
             $lanes = LaneFactory::createMany(3, ['board' => $board]);
 
+            // For each lane, it generates between 3 and 7 associated cards.
             foreach ($lanes as $lane) {
                 $count = random_int(3, 7);
                 CardFactory::createMany($count, ['lane' => $lane]);
