9c94e35d01a0c1012d78f17b9c5b55d768f42490
Baylo <microsoftbaylo@laposte.net>
2025-08-25 15:42:25 +0200

    feat: enhance AcceptLanguageListener to set locale based on Accept-Language header

--- src/EventListener/AcceptLanguageListener.php
+++ src/EventListener/AcceptLanguageListener.php
@@ -4,13 +4,28 @@ namespace App\EventListener;
 
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 
-#[AsEventListener]
+/*
+ * This listener is responsible for setting the locale based on the Accept-Language header
+ * sent by the client. It runs after the default locale (priority 16).
+ */
+#[AsEventListener(event: KernelEvents::REQUEST, priority: 32)] // 32 to run after the default locale is set
 final class AcceptLanguageListener
 {
+    public const MANAGED_LOCALES = ['en', 'fr'];
 
     public function __invoke(RequestEvent $event): void
     {
-        // ...
+        $request = $event->getRequest();
+
+        // Detect the best language from the user's Accept-Language header
+        $preferredLocale = $request->getPreferredLanguage(self::MANAGED_LOCALES);
+
+        if ($preferredLocale) {
+            // Apply the detected locale to the current request
+            $request->setLocale($preferredLocale);
+        }
     }
 }
+
