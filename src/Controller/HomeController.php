164a885b3f754d9c098839728ae6739033342a5c
Baylo <microsoftbaylo@laposte.net>
2025-08-26 14:46:13 +0200

    feat: enhance index method in HomeController to include caching and ETag handling

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,12 +7,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Attribute\Cache;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     #[Cache(expires: 'tomorrow midnight', maxage: 3600, public: true)]
-    public function index(Request $request): Response
+    public function index(Request $request, CacheInterface $cache): Response
     {
         $response = new Response();
         // The ETag is a string that must be unique for each version of the document.
@@ -23,7 +25,18 @@ final class HomeController extends AbstractController
             // Response without body with status "304 Not Modified"
             return $response;
         }
+        $cachedData = $cache->get('key', function (ItemInterface $item) {
+            $item->expiresAfter(3600);
 
-        return $this->render('home/index.html.twig');
+            usleep(500000);
+
+            return 'the data produced by an expensive computation';
+        });
+
+
+
+        return $this->render('default/index.html.twig', [
+            'data' => $cachedData,
+        ]);
     }
 }
