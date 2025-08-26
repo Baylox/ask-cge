4000fc0e1ee6c0e2e3711f30bd9330d1c3613d19
Baylo <microsoftbaylo@laposte.net>
2025-08-26 11:37:19 +0200

    feat: update HomeController to include ETag handling and cache settings

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\Cache;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Cache(expires: 'tomorrow midnight', maxage: 3600, public: true)]
+    public function index(Request $request): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $response = new Response();
+        // The ETag is a string that must be unique for each version of the document.
+        // You are completely free to calculate it as you wish.
+        // We'll choose a low-cost method -
+        $response->setEtag($request->getLocale());
+        if ($response->isNotModified($request)) {
+            // Response without body with status "304 Not Modified"
+            return $response;
+        }
+
+        return $this->render('default/index.html.twig');
     }
 }
