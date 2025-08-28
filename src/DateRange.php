499b1367cb80786cc7d47c1f4b47f15714003999
Baylo <microsoftbaylo@laposte.net>
2025-08-28 10:07:25 +0200

    feat: add createdAt field to BoardType form and implement DateRange class for date handling

---
+++ src/DateRange.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+class DateRange
+{
+    private \DateTimeInterface $from;
+    private \DateTimeInterface $to;
+
+    public function __construct(\DateTimeInterface $from, \DateTimeInterface $to)
+    {
+        $this->from = $from;
+        $this->to = $to;
+    }
+
+    public function getFrom(): \DateTimeInterface
+    {
+        return $this->from;
+    }
+
+    public function getTo(): \DateTimeInterface
+    {
+        return $this->to;
+    }
+}
--- src/Form/BoardType.php
+++ src/Form/BoardType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
 
 class BoardType extends AbstractType
@@ -17,15 +18,16 @@ class BoardType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('state', EnumType::class,['class' => BoardState::class],)
+            ->add('state', EnumType::class, ['class' => BoardState::class],)
             ->add('accounts', EntityType::class, [
                 'class' => Account::class,
-                'choice_label' => 'id',
+                'choice_label' => 'email',
                 'multiple' => true,
             ])
-            ->add('createdAt', null, [
-            'widget' => 'choice',
-        ])
+            ->add('createdAt', DateType::class, [
+                'widget' => 'choice',    // listes déroulantes
+                'format' => 'dd-MM-yyyy' // ordre jour-mois-année
+            ])
         ;
     }
 
