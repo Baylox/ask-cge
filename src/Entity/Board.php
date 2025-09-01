7f6c3d8e7b4c3036ffd481dd526aa0368680e51b
Baylo <microsoftbaylo@laposte.net>
2025-09-02 01:38:43 +0200

    fix: add validation constraint for title in Board entity

--- src/Entity/Board.php
+++ src/Entity/Board.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\UX\Turbo\Attribute\Broadcast;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: BoardRepository::class)]
 // #[Broadcast]
@@ -18,6 +19,7 @@ class Board
     private ?int $id = null;
 
     #[ORM\Column(length: 50)]
+    #[Assert\NotBlank(message: "Title is required.")]
     private ?string $title = null;
 
     /**
